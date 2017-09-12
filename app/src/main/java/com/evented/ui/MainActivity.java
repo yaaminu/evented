35c86036dde1e5dacecd5baa8fc0265af9fd3d24
yaaminu <afolanyaaminu@gmail.com>
2017-09-12 15:03:11 +0000

    pull out test data generator from MainActivity

--- app/src/main/java/com/evented/ui/MainActivity.java
+++ app/src/main/java/com/evented/ui/MainActivity.java
@@ -20,25 +20,17 @@ import android.widget.ListView;
 import android.widget.TextView;
 
 import com.evented.R;
-import com.evented.events.data.BillingAcount;
 import com.evented.events.data.Event;
-import com.evented.events.data.EventBuilder;
 import com.evented.events.data.NearbyEvents;
-import com.evented.events.data.TicketType;
-import com.evented.events.data.User;
 import com.evented.events.data.UserManager;
-import com.evented.events.data.Venue;
 import com.evented.utils.GenericUtils;
 import com.evented.utils.PLog;
 import com.evented.utils.ViewUtils;
 import com.rey.slidelayout.SlideLayout;
 
-import java.security.SecureRandom;
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.Collections;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -48,7 +40,6 @@ import butterknife.OnItemClick;
 import butterknife.OnTextChanged;
 import io.realm.Case;
 import io.realm.Realm;
-import io.realm.RealmList;
 import io.realm.RealmResults;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.functions.Action1;
@@ -106,7 +97,6 @@ public class MainActivity extends AppCompatActivity implements SimpleEventListFr
                 .beginTransaction()
                 .replace(R.id.fragment_container, currentFragment, TAG_EVENT_LIST)
                 .commit();
-        createEvents();
         slide_layout.setOnStateChangedListener(stateChangedListener);
         int[] icons = {
                 R.drawable.ic_trending_events_24dp,
@@ -326,210 +316,4 @@ public class MainActivity extends AppCompatActivity implements SimpleEventListFr
                 });
     }
 
-    static void createEvents() {
-        final String description = "This is the description of the event. It must give precise information of what " +
-                "the event is all about and all other valuable information";
-        final RealmList<TicketType> ticketTypes = new RealmList<>(new TicketType("VIP", 1000, 1000));
-
-        new Thread() {
-
-            private final Venue address = new Venue("Silverbird Cinemas Accra", "address", -0.17353469999999996, 5.6218986);
-
-            @Override
-            public void run() {
-
-                Realm realm = Realm.getDefaultInstance();
-                try {
-                    if (realm.where(Event.class)
-                            .count() > 20) {
-                        return;
-                    }
-                    User currentUser = UserManager.getInstance().getCurrentUser();
-                    if (currentUser == null) {
-                        currentUser = new User("233266349205", "233266349205", true);
-                    }
-                    realm.beginTransaction();
-                    createToday(currentUser, realm);
-                    createTomorrow(realm, currentUser);
-                    createEventNextWeek(realm, currentUser);
-                    createEventNextMonth(realm, currentUser);
-                    createEventAfterNextMonth(realm, currentUser);
-                    realm.commitTransaction();
-                } finally {
-                    realm.close();
-                }
-            }
-
-            private void createToday(User currentUser, Realm realm) {
-                List<Event> events = new ArrayList<>(20);
-                BillingAcount billingAcount =
-                        new BillingAcount("yaaminu",
-                                currentUser.phoneNumber, "MTN");
-
-
-                for (int i = 0; i < 20; i++) {
-                    events.add(new EventBuilder()
-                            .setBillingAcount(billingAcount)
-                            .setCategory(Math.max(1, i % 9))
-                            .setCreatedBy(currentUser.userId)
-                            .setDateCreated(System.currentTimeMillis())
-                            .setDateUpdated(System.currentTimeMillis())
-                            .setStartDate(System.currentTimeMillis())
-                            .setEndDate(System.currentTimeMillis() + TimeUnit.DAYS.toMillis(4))
-                            .setEventId("eventIdToday" + i)
-                            .setDescription(description)
-                            .setLikes(Math.abs(new SecureRandom().nextInt()) % 100)
-                            .setGoing(Math.abs(new SecureRandom().nextInt()) % 100)
-                            .setPublicity(Event.PUBLICITY_PUBLIC)
-                            .setName("Event name " + i)
-                            .setVenue(new Venue("Silverbird Cinemas Accra", "address",
-                                    -0.17353469999999996 + new SecureRandom().nextDouble() % 0.1,
-                                    5.6218986 + new SecureRandom().nextDouble() % 0.1))
-                            .createEvent());
-                    events.get(i).setLiked(i % 5 == 0);
-                    events.get(i).setCurrentUserGoing(i % 2 == 0);
-                    events.get(i).setWebLink("https://facebook.com");
-                    events.get(i).setOrganizerContact("0266349205");
-                    events.get(i).setTicketTypes(ticketTypes);
-                }
-                realm.copyToRealmOrUpdate(events);
-            }
-
-            private void createTomorrow(Realm realm, User currentUser) {
-                BillingAcount billingAcount =
-                        new BillingAcount("username",
-                                currentUser.phoneNumber, "MTN");
-                List<Event> events = new ArrayList<>(20);
-                for (int i = 0; i < 20; i++) {
-                    events.add(new EventBuilder()
-                            .setCreatedBy(currentUser.userId)
-                            .setCategory(Math.max(1, i % 9))
-                            .setBillingAcount(billingAcount)
-                            .setDateCreated(System.currentTimeMillis())
-                            .setDateUpdated(System.currentTimeMillis())
-                            .setStartDate(System.currentTimeMillis() + TimeUnit.DAYS.toMillis(1))
-                            .setEndDate(System.currentTimeMillis() + TimeUnit.DAYS.toMillis(4))
-                            .setEventId("eventIdTomorrow" + i)
-                            .setDescription(description)
-                            .setGoing(Math.abs(new SecureRandom().nextInt()) % 100)
-                            .setLikes(Math.abs(new SecureRandom().nextInt()) % 100)
-                            .setPublicity(Event.PUBLICITY_PUBLIC)
-                            .setName("Event name " + i)
-                            .setGoing(Math.abs(new SecureRandom().nextInt()) % 100)
-                            .setVenue(address)
-                            .createEvent());
-                    events.get(i).setLiked(i % 5 == 0);
-                    events.get(i).setCurrentUserGoing(i % 2 == 0);
-                    events.get(i).setTicketTypes(ticketTypes);
-                    events.get(i).setWebLink("https://facebook.com");
-                    events.get(i).setOrganizerContact("0266349205");
-                }
-                realm.copyToRealmOrUpdate(events);
-            }
-
-            private void createEventNextWeek(Realm realm, User currentUser) {
-                List<Event> events = new ArrayList<>(20);
-                BillingAcount billingAcount =
-                        new BillingAcount("username",
-                                currentUser.phoneNumber, "MTN");
-                for (int i = 0; i < 20; i++) {
-                    events.add(new EventBuilder()
-                            .setCreatedBy(currentUser.userId)
-                            .setDateCreated(System.currentTimeMillis())
-                            .setDateUpdated(System.currentTimeMillis())
-                            .setStartDate(System.currentTimeMillis() + TimeUnit.DAYS.toMillis(3))
-                            .setEndDate(System.currentTimeMillis() + TimeUnit.DAYS.toMillis(11))
-                            .setEventId("eventIdNextWeek" + i)
-                            .setCategory(Math.max(1, i % 9))
-                            .setDescription(description)
-                            .setBillingAcount(billingAcount)
-                            .setPublicity(Event.PUBLICITY_PUBLIC)
-                            .setGoing(Math.abs(new SecureRandom().nextInt()) % 100)
-                            .setLikes(Math.abs(new SecureRandom().nextInt()) % 100)
-                            .setName("Event name " + i)
-                            .setVenue(address)
-                            .createEvent());
-                    events.get(i).setLiked(i % 5 == 0);
-                    events.get(i).setCurrentUserGoing(i % 2 == 0);
-                    events.get(i).setTicketTypes(ticketTypes);
-                    events.get(i).setWebLink("https://facebook.com");
-                    events.get(i).setOrganizerContact("0266349205");
-                }
-                realm.copyToRealmOrUpdate(events);
-            }
-
-            private void createEventNextMonth(Realm realm, User currentUser) {
-                List<Event> events = new ArrayList<>(20);
-                BillingAcount billingAcount =
-                        new BillingAcount("username",
-                                currentUser.phoneNumber, "MTN");
-                for (int i = 0; i < 20; i++) {
-                    events.add(new EventBuilder()
-                            .setCreatedBy(currentUser.userId)
-                            .setDateCreated(System.currentTimeMillis())
-                            .setBillingAcount(billingAcount)
-                            .setCategory(Math.max(1, i % 9))
-                            .setDateUpdated(System.currentTimeMillis())
-                            .setStartDate(System.currentTimeMillis() + TimeUnit.DAYS.toMillis(33))
-                            .setEndDate(System.currentTimeMillis() + TimeUnit.DAYS.toMillis(44))
-                            .setEventId("eventIdNextMonth" + i)
-                            .setDescription(description)
-                            .setLikes(Math.abs(new SecureRandom().nextInt()) % 100)
-                            .setGoing(Math.abs(new SecureRandom().nextInt()) % 100)
-                            .setPublicity(Event.PUBLICITY_PUBLIC)
-                            .setName("Event name " + i)
-                            .setVenue(address)
-                            .createEvent());
-                    events.get(i).setLiked(i % 5 == 0);
-                    events.get(i).setCurrentUserGoing(i % 2 == 0);
-                    events.get(i).setWebLink("https://facebook.com");
-                    events.get(i).setOrganizerContact("0266349205");
-                    events.get(i).setTicketTypes(ticketTypes);
-                }
-                realm.copyToRealmOrUpdate(events);
-            }
-
-            private void createEventAfterNextMonth(Realm realm, User currentUser) {
-                List<Event> events = new ArrayList<>(20);
-                BillingAcount billingAcount =
-                        new BillingAcount("username",
-                                currentUser.phoneNumber, "MTN");
-
-                Calendar calendar = Calendar.getInstance();
-                calendar.set(Calendar.DAY_OF_MONTH, 1);
-                calendar.set(Calendar.HOUR_OF_DAY, 0);
-                calendar.set(Calendar.MINUTE, 0);
-                calendar.set(Calendar.SECOND, 0);
-                calendar.set(Calendar.MILLISECOND, 0);
-                int afterNextMonth = calendar.get(Calendar.MONTH) + 2;
-                int maxMonth = calendar.getActualMaximum(Calendar.MONTH);
-                calendar.set(Calendar.MONTH, afterNextMonth > maxMonth ? (afterNextMonth - maxMonth) - 1/*start over*/ : afterNextMonth);
-
-                for (int i = 0; i < 20; i++) {
-                    events.add(new EventBuilder()
-                            .setCreatedBy(currentUser.userId)
-                            .setDateCreated(System.currentTimeMillis())
-                            .setDateUpdated(System.currentTimeMillis())
-                            .setStartDate(calendar.getTimeInMillis())
-                            .setEndDate(calendar.getTimeInMillis() + TimeUnit.DAYS.toMillis(3))
-                            .setEventId("eventIdMore" + i)
-                            .setDescription(description)
-                            .setCategory((1 + i) % 9)
-                            .setLikes(Math.abs(new SecureRandom().nextInt()) % 100)
-                            .setPublicity(Event.PUBLICITY_PUBLIC)
-                            .setBillingAcount(billingAcount)
-                            .setGoing(Math.abs(new SecureRandom().nextInt()) % 100)
-                            .setName("Event name " + i)
-                            .setVenue(address)
-                            .createEvent());
-                    events.get(i).setLiked(i % 5 == 0);
-                    events.get(i).setCurrentUserGoing(i % 2 == 0);
-                    events.get(i).setTicketTypes(ticketTypes);
-                    events.get(i).setWebLink("https://facebook.com");
-                    events.get(i).setOrganizerContact("0266349205");
-                }
-                realm.copyToRealmOrUpdate(events);
-            }
-        }.start();
-    }
 }
